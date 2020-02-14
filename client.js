56af9c5c2c1de1d062a7fb71a7774bddb731f3ec
Manish <mp121995@gmail.com>
2020-02-14 12:45:30 -0800

    Axioms

--- client.js
+++ client.js
@@ -1,1 +1,18 @@
 const axios = require('axios');
+
+axios.get('http://localhost:3000')
+.then((res)=>{
+    console.log("Success", res.data);
+})
+.catch(e => console.log(e));
+
+const body = {
+    hello: "Manish",
+    class : "667"
+}
+
+axios.post('http://localhost:3000/post-test',body)
+.then((res)=>{
+    console.log("Success", res.data);
+})
+.catch(e => console.log(e));
